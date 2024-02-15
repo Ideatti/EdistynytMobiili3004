5492a182c29c7aa7d7858af513f3d10476600949
Ideatti <108632253+Ideatti@users.noreply.github.com>
2024-02-15 12:50:02 +0200

    lot of new stuff, fifth study CGTS VM added to screen

--- app/src/main/java/com/example/edistynytmobiili3004/CategoriesScreen.kt
+++ app/src/main/java/com/example/edistynytmobiili3004/CategoriesScreen.kt
@@ -1,9 +1,19 @@
 package com.example.edistynytmobiili3004
 
+import android.inputmethodservice.Keyboard
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -11,19 +21,54 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.edistynytmobiili3004.viewmodel.CategoriesViewModel
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CategoriesScreen(onMenuClick: () -> Unit){
+fun CategoriesScreen(onMenuClick: () -> Unit) {
+    val categoriesVm: CategoriesViewModel = viewModel()
+
     Scaffold(topBar = {
         TopAppBar(title = { Text(text = "Categories") }, navigationIcon = {
             IconButton(onClick = { onMenuClick() }) {
                 Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
-                
             }
         })
-    }) {
-        LazyColumn(modifier = Modifier.padding(it)){}
+    }) {paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            when {
+                categoriesVm.categoriesState.value.loading -> CircularProgressIndicator(
+                    modifier = Modifier.align(
+                        Alignment.Center
+                    )
+                )
+
+                else -> LazyColumn(){
+                    items(categoriesVm.categoriesState.value.list){
+                        Column(modifier = Modifier.fillMaxWidth()) {
+                            Row (Modifier.fillMaxWidth().padding(8.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween) {
+                                Text(text = "Kuva")
+                                Column (horizontalAlignment = Alignment.End) {
+                                    Text(text = it.name, fontWeight = FontWeight.ExtraBold)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/edistynytmobiili3004/viewmodel/CategoriesViewModel.kt
+++ app/src/main/java/com/example/edistynytmobiili3004/viewmodel/CategoriesViewModel.kt
@@ -1,9 +1,41 @@
 package com.example.edistynytmobiili3004.viewmodel
 
+import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.edistynytmobiili3004.model.CategoriesState
+import com.example.edistynytmobiili3004.model.CategoryItem
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class CategoriesViewModel : ViewModel () {
 
+    private val _categoriesState = mutableStateOf(CategoriesState())
+    val categoriesState: State<CategoriesState> = _categoriesState
 
+    init {
+        getCategories()
+    }
 
+    private suspend fun waitForCategories(){
+        delay( 2000)
+    }
+
+    fun getCategories(){
+        viewModelScope.launch {
+            Log.d("tomi", "in categories: fetch data")
+            _categoriesState.value = _categoriesState.value.copy(loading = true)
+            waitForCategories()
+            _categoriesState.value = categoriesState.value.copy(
+                loading = false,
+                list= listOf(
+                    CategoryItem(id=1, name= "Kategoria1"),
+                    CategoryItem(id=2, name= "Kategoria2")
+                )
+            )
+            Log.d("tomi", "in categories: data fetched")
+        }
+    }
 }
